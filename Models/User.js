45b4ae4182c35df56a2357b1b881f2de7fc5e111
anonymerd <rhtbisht738@gmail.com>
2021-06-26 03:58:47 +0530

    Created User Model and Intergrated Firebase

--- Models/User.js
+++ Models/User.js
@@ -1,8 +1,61 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('../Config/Firebase.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const FieldValue = admin.firestore.FieldValue;
+const db = admin.firestore();
+
 class User {
-  constructor(name, email) {
+  constructor(id, name, email) {
+    this.id = id;
     this.name = name;
     this.email = email;
+    this.likedSongs = [];
   }
+
+  checkInFireStore = async () => {
+    try {
+      const userRef = db.collection('users').doc(this.id);
+      const doc = await userRef.get();
+      return doc.exists;
+    } catch (error) {
+      console.error('Unable to run checkInFireStore! ' + error);
+    }
+  };
+
+  addToFirestore = async () => {
+    try {
+      if (await this.checkInFireStore()) {
+        const userRef = db.collection('users').doc(this.id);
+        await userRef.set({
+          name: this.name,
+          email: this.email,
+          likedSongs: this.likedSongs,
+        });
+      } else {
+        console.log('User is already present in Firestore!');
+      }
+    } catch (error) {
+      console.error('Unable to add User! ' + error);
+    }
+  };
+
+  addLikedSong = async (songId) => {
+    const userRef = db.collection('users').doc(this.songId);
+    await userRef.update({
+      likedSongs: FieldValue.arrayUnion(songId),
+    });
+  };
+
+  removeLikedSong = async (songId) => {
+    const userRef = db.collection('users').doc(this.songId);
+    await userRef.update({
+      likedSongs: FieldValue.arrayRemove(songId),
+    });
+  };
 }
 
 export default User;
--- Routes/likedSongsRoute.js
+++ Routes/likedSongsRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const likedSongsController = require('../controllers/likedSongsController');
+const likedSongsController = require('../Controllers/likedSongsController');
 
 const likedSongsRouter = express.Router();
 likedSongsRouter.route('/').post(likedSongsController.storeLikedSongs);
