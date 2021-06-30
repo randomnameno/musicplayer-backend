d9028f9d2cffa55510a7494b17a7104ca956a420
anonymerd <rhtbisht738@gmail.com>
2021-06-30 22:38:05 +0530

    Added authorization in APIs

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,46 +1,120 @@
 const User = require('../Models/User');
+const { verifyGoogleToken } = require('../Config/verifyToken');
+const YoutubeMusicApi = require('youtube-music-api');
 
 const addToLikedSongs = async (req, res) => {
-  try {
-    const data = req.body;
+  // Getting the token.
+  if (!req.headers['authorization']) {
+    // If Authorization header is not found then sending back the response.
+    res.json({
+      success: false,
+      message: 'Authorization header is not present.',
+    });
+  } else {
+    const token = req.headers['authorization'].split(' ')[1];
 
-    // Creating new User
-    const user = await User.getUserInfo(data.email);
+    // Verifying the token.
+    const payload = await verifyGoogleToken(token);
 
-    // Adding song to user's liked song list.
-    await user.addLikedSong(data.songId);
-    res.json({ success: true, message: 'Added to Liked Songs' });
-  } catch (error) {
-    console.error('The error is in addToLikedSongs: ' + error);
-    res.json({ success: false, message: error });
+    if (payload.email_verified) {
+      try {
+        const data = req.body;
+
+        // Creating new User
+        const user = await User.getUserInfo(payload.email);
+
+        // Adding song to user's liked song list.
+        await user.addLikedSong(data.songId);
+        res.json({ success: true, message: 'Added to Liked Songs' });
+      } catch (error) {
+        console.error('The error is in addToLikedSongs: ' + error);
+        res.json({ success: false, message: error });
+      }
+    }
   }
 };
 
 const removeFormLikedSongs = async (req, res) => {
-  try {
-    const data = req.body;
+  // Getting the token.
+  if (!req.headers['authorization']) {
+    // If Authorization header is not found then sending back the response.
+    res.json({
+      success: false,
+      message: 'Authorization header is not present.',
+    });
+  } else {
+    const token = req.headers['authorization'].split(' ')[1];
 
-    // Creating new User
-    const user = await User.getUserInfo(data.email);
+    // Verifying the token.
+    const payload = await verifyGoogleToken(token);
 
-    // Removing song from user's liked song list
-    await user.removeLikedSong(data.songId);
-    res.json({ success: true, message: 'Removed From Liked Songs' });
-  } catch (error) {
-    console.error('The error is in removeFromLikedSongs: ' + error);
-    res.json({ success: false, message: error });
+    if (payload.email_verified) {
+      try {
+        const data = req.body;
+
+        // Creating new User
+        const user = await User.getUserInfo(payload.email);
+
+        // Removing song from user's liked song list
+        await user.removeLikedSong(data.songId);
+        res.json({ success: true, message: 'Removed From Liked Songs' });
+      } catch (error) {
+        console.error('The error is in removeFromLikedSongs: ' + error);
+        res.json({ success: false, message: error });
+      }
+    }
   }
 };
-
 const getLikedSongs = async (req, res) => {
-  try {
-  } catch (error) {
-    console.error('The error is in getLikedSongs: ' + error);
-    res.json({ success: false, message: error });
+  // Getting the token.
+  if (!req.headers['authorization']) {
+    // If Authorization header is not found then sending back the response.
+    res.json({
+      success: false,
+      message: 'Authorization header is not present.',
+    });
+  } else {
+    const token = req.headers['authorization'].split(' ')[1];
+
+    // Verifying the token.
+    const payload = await verifyGoogleToken(token);
+
+    if (payload.email_verified) {
+      try {
+        // Getting the user info
+        const user = await User.getUserInfo(payload.email);
+
+        // Using Youtube Music API to fetch song data from Youtube.
+        const api = new YoutubeMusicApi();
+        await api.initalize();
+
+        const likedSongs = await Promise.all(
+          user.likedSongs.map(async (item) => {
+            let currSong = await api.search(item, 'song');
+            currSong = currSong.content[0];
+            return {
+              song: currSong.name,
+              artist: currSong.artist.name,
+              thumbnail: currSong.thumbnails[1],
+              streamAddress:
+                'http://localhost:8000/api/stream/' + currSong.videoId,
+              songId: currSong.videoId,
+              isLiked: true,
+            };
+          })
+        );
+
+        res.json({ success: true, likedSongs: likedSongs.reverse() });
+      } catch (error) {
+        console.error('The error is in getLikedSongs: ' + error);
+        res.json({ success: false, message: error.toString() });
+      }
+    }
   }
 };
 
 module.exports = {
   addToLikedSongs: addToLikedSongs,
   removeFormLikedSongs: removeFormLikedSongs,
+  getLikedSongs: getLikedSongs,
 };
--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -5,6 +5,7 @@ const userController = require('../Controllers/userController');
 const userRouter = express.Router();
 userRouter
   .route('/like')
+  .get(userController.getLikedSongs)
   .put(userController.addToLikedSongs)
   .delete(userController.removeFormLikedSongs);
 
